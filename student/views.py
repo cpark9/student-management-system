b612134edf691b94afe02c944397709f2c4e26ad
cpark9 <cpark9.gmail.com>
2026-01-19 11:13:48 +0900

    26-01-19

--- student/views.py
+++ student/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def add_student(request):
+
+    return render(request,"students/add-student.html")
+
+def student_list(request):
+    return render(request,"students/student.html")
+
+def edit_student(request):
+    return render(request,"students/edit-student.html")
+
+def view_student(request):
+    return render(request,"students/student.html")
+
+
+
+
+
